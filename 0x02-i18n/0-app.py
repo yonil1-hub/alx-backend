795c8f72d7c706a7bf1e34f6abfa448a7f2cdb99
yonil1-hub <wyml@yahoo.com>
2023-04-28 17:00:33 +0000

    render the template

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+A basic Flask app that renders an index.html template
+"""
+
+from flask import Flask, render_template
+
+
+# Create an instance of the Flask application
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index():
+    """
+    Renders the index.html template
+    """
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
